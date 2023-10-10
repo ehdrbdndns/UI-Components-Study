10702f95f7cba616221c5743c0cda2310741a532
ehdrbdndns <60888473+ehdrbdndns@users.noreply.github.com>
2023-10-10 20:58:30 +0900

    파일 재 구조화

---
+++ Carousel/3D Carousel/index.ts
@@ -0,0 +1,8 @@
+// tsc script.ts --watch
+function sayHelloWorld(): void {
+  console.log('\n');
+  console.log('*** Hello World from Type Script code. ***');
+  console.log('\n');
+}
+
+sayHelloWorld();
