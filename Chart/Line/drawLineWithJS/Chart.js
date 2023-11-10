ab113eda2e6d0099decd47c5d00ee905199721ed
ehdrbdndns <60888473+ehdrbdndns@users.noreply.github.com>
2023-11-10 22:38:52 +0900

    JS 기본 구조 셋팅

---
+++ Chart/Line/drawLineWithJS/Chart.js
@@ -0,0 +1,13 @@
+(function () {
+  // Set Container
+  const setContainer = () => {};
+  // Set Axes
+  const setAxes = () => {};
+  // Set Label
+  const setLabel = () => {};
+  // Set Data
+  const setData = () => {};
+
+  // rendering for chart
+  const render = () => {};
+})();
