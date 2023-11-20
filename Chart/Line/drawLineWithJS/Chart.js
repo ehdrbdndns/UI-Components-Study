0f2688a24d29a32a138786fa50708fc0b18d7407
ehdrbdndns <60888473+ehdrbdndns@users.noreply.github.com>
2023-11-20 19:45:47 +0900

    gradient color & label & guide line 추가

--- Chart/Line/drawLineWithJS/Chart.js
+++ Chart/Line/drawLineWithJS/Chart.js
@@ -14,15 +14,17 @@ var Chart = /** @class */ (function () {
         var _this = this;
         this.svgNs = 'http://www.w3.org/2000/svg';
         this.padding = { x: 0, y: 0 };
+        this.yAxisCount = 10;
         this.maxData = 0; // y축에서 표현되는 가장 큰 수
+        this.minData = 0; // y축에서 표현되는 가장 작은 수
         this.setSVGPadding = function () {
             // 1. Y-Padding
             // find max y-label length
             // mix font-size and y-label length
-            _this.padding = __assign(__assign({}, _this.padding), { y: _this.fontSize + Math.ceil(Math.log(_this.maxData + 1) / Math.LN10) * 10 });
             // 2. X-Padding
-            // mix font-size and datas.length
-            _this.padding = __assign(__assign({}, _this.padding), { x: _this.fontSize + _this.datas.length * 30 });
+            _this.padding = __assign(__assign({}, _this.padding), { 
+                // mix font-size and datas.length
+                x: _this.fontSize + _this.datas.length * 30, y: _this.fontSize + Math.ceil(Math.log(_this.maxData + 1) / Math.LN10) * 10 });
         };
         this.setSVGElement = function () {
             // Make SVG Container
@@ -63,28 +65,107 @@ var Chart = /** @class */ (function () {
             _this.setAxis();
         };
         this.setPoints = function () {
+            // set Color
+            var defsTagOfColor = document.createElementNS(_this.svgNs, 'defs');
+            var linearGradientTag = document.createElementNS(_this.svgNs, 'linearGradient');
+            var stop1 = document.createElementNS(_this.svgNs, 'stop');
+            var stop2 = document.createElementNS(_this.svgNs, 'stop');
+            linearGradientTag.setAttribute('id', 'paint1');
+            linearGradientTag.setAttribute('gradientTransform', 'rotate(90)');
+            stop1.setAttribute('stop-color', '#FA00FF');
+            stop2.setAttribute('offset', '1');
+            stop2.setAttribute('stop-color', '#0085FF');
+            linearGradientTag.appendChild(stop1);
+            linearGradientTag.appendChild(stop2);
+            defsTagOfColor.appendChild(linearGradientTag);
+            _this.chart.appendChild(defsTagOfColor);
             // make g container
             var gTagOfPolyLine = document.createElementNS(_this.svgNs, 'g');
             gTagOfPolyLine.classList.add('datas');
             for (var i = 0; i < _this.datas.length; i++) {
                 var points = _this.datas[i].data
                     .map(function (value, j) {
-                    var x = ((j + 1) / _this.xAxisCount) * (_this.width - _this.padding.y);
-                    var y = (_this.hegiht - _this.padding.x) * (value / _this.maxData);
+                    var x = (j / (_this.xAxisCount - 1)) * (_this.width - _this.padding.y) +
+                        _this.padding.y;
+                    var y = _this.hegiht -
+                        _this.padding.x -
+                        (_this.hegiht - _this.padding.x) * (value / _this.maxData);
                     return "".concat(x, ",").concat(y);
                 })
                     .join(' ');
                 // draw polylines
                 var polyLine = document.createElementNS(_this.svgNs, 'polyline');
                 polyLine.setAttribute('points', points);
-                polyLine.setAttribute('stroke', _this.datas[i].color);
+                if (i === 0) {
+                    polyLine.setAttribute('stroke', "url('#paint1')");
+                }
+                else {
+                    polyLine.setAttribute('stroke', _this.datas[i].color);
+                }
                 polyLine.setAttribute('fill', 'none');
                 polyLine.setAttribute('stroke-width', _this.datas[i].weight + '');
+                polyLine.setAttribute('stroke-linecap', 'round');
+                polyLine.setAttribute('stroke-linejoin', 'round');
                 gTagOfPolyLine.appendChild(polyLine);
             }
             _this.chart.appendChild(gTagOfPolyLine);
         };
-        this.setLabel = function () { };
+        this.setLabel = function () {
+            var gTagOfText = document.createElementNS(_this.svgNs, 'g');
+            var gTagOfXLabel = document.createElementNS(_this.svgNs, 'g');
+            var gTagOfYLabel = document.createElementNS(_this.svgNs, 'g');
+            gTagOfText.setAttribute('fill', '#fff');
+            gTagOfText.setAttribute('font-size', _this.fontSize + 'px');
+            gTagOfText.classList.add('labels');
+            gTagOfXLabel.setAttribute('text-anchor', 'end');
+            gTagOfYLabel.setAttribute('text-anchor', 'end');
+            // xLabel
+            _this.labels.map(function (label, i) {
+                var x = (i / (_this.xAxisCount - 1)) * (_this.width - _this.padding.y) +
+                    _this.padding.y;
+                var y = _this.hegiht - _this.padding.x + _this.fontSize * 2;
+                var text = document.createElementNS(_this.svgNs, 'text');
+                text.setAttribute('x', x + '');
+                text.setAttribute('y', y + '');
+                text.append(label);
+                gTagOfXLabel.appendChild(text);
+            });
+            gTagOfText.appendChild(gTagOfXLabel);
+            // yLabel
+            // 1. 10개의 y lable 데이터 생성
+            // 2. x, y좌표 생성
+            for (var i = 0; i <= _this.yAxisCount; i++) {
+                var x = _this.padding.x - Math.ceil(Math.log(_this.maxData + 1) / Math.LN10);
+                var y = (_this.hegiht - _this.padding.x) * (i / _this.yAxisCount);
+                var label = ((_this.yAxisCount - i) / _this.yAxisCount) *
+                    (_this.maxData - _this.minData) +
+                    _this.minData;
+                var text = document.createElementNS(_this.svgNs, 'text');
+                text.setAttribute('x', x + '');
+                text.setAttribute('y', y + '');
+                text.append(label + '');
+                gTagOfYLabel.appendChild(text);
+            }
+            gTagOfText.appendChild(gTagOfYLabel);
+            _this.chart.appendChild(gTagOfText);
+        };
+        this.setGuideLine = function () {
+            var gTagOfLine = document.createElementNS(_this.svgNs, 'g');
+            gTagOfLine.setAttribute('stroke', '#fff');
+            gTagOfLine.setAttribute('stroke-weight', '1');
+            for (var i = 0; i <= _this.yAxisCount; i++) {
+                var x1 = _this.padding.y;
+                var x2 = _this.width;
+                var y = (_this.hegiht - _this.padding.x) * (i / _this.yAxisCount);
+                var line = document.createElementNS(_this.svgNs, 'line');
+                line.setAttribute('x1', x1 + '');
+                line.setAttribute('x2', x2 + '');
+                line.setAttribute('y1', y + '');
+                line.setAttribute('y2', y + '');
+                gTagOfLine.appendChild(line);
+            }
+            _this.chart.appendChild(gTagOfLine);
+        };
         // rendering for chart
         this.render = function () {
             var _a;
@@ -93,6 +174,11 @@ var Chart = /** @class */ (function () {
             // 데이터 구축
             _this.setPoints();
             // 데이터 라벨링
+            _this.setLabel();
+            // 가이드 라인
+            _this.setGuideLine();
+            // 라벨 박스
+            // last point
             (_a = document.getElementById(_this.targetId)) === null || _a === void 0 ? void 0 : _a.appendChild(_this.chart);
         };
         var datas = data.datas, size = data.size, targetId = data.targetId, labels = data.labels;
@@ -105,6 +191,8 @@ var Chart = /** @class */ (function () {
         this.labels = labels;
         this.xAxisCount = labels.length;
         this.maxData = Math.max.apply(Math, datas.map(function (data) { return data.max; }));
+        this.minData = Math.min.apply(Math, datas.map(function (data) { return data.min; }));
+        console.log(datas);
     }
     return Chart;
 }());
--- Chart/Line/drawLineWithJS/Chart.ts
+++ Chart/Line/drawLineWithJS/Chart.ts
@@ -46,7 +46,9 @@ class Chart {
   private datas: ChartDataType[];
   private labels: string[]; // x축에서 표현되는 라벨 들
   private xAxisCount: number; // x축에서 표현되는 라벨의 개수
+  private yAxisCount: number = 10;
   private maxData: number = 0; // y축에서 표현되는 가장 큰 수
+  private minData: number = 0; // y축에서 표현되는 가장 작은 수
 
   constructor(data: ChartType) {
     const { datas, size, targetId, labels } = data;
@@ -61,22 +63,21 @@ class Chart {
     this.labels = labels;
     this.xAxisCount = labels.length;
     this.maxData = Math.max(...datas.map((data) => data.max));
+    this.minData = Math.min(...datas.map((data) => data.min));
+
+    console.log(datas);
   }
 
   private setSVGPadding = () => {
     // 1. Y-Padding
     // find max y-label length
     // mix font-size and y-label length
-    this.padding = {
-      ...this.padding,
-      y: this.fontSize + Math.ceil(Math.log(this.maxData + 1) / Math.LN10) * 10,
-    };
-
     // 2. X-Padding
-    // mix font-size and datas.length
     this.padding = {
       ...this.padding,
+      // mix font-size and datas.length
       x: this.fontSize + this.datas.length * 30,
+      y: this.fontSize + Math.ceil(Math.log(this.maxData + 1) / Math.LN10) * 10,
     };
   };
 
@@ -125,15 +126,44 @@ class Chart {
   };
 
   private setPoints = () => {
+    // set Color
+    const defsTagOfColor = document.createElementNS(this.svgNs, 'defs');
+    const linearGradientTag = document.createElementNS(
+      this.svgNs,
+      'linearGradient'
+    );
+    const stop1 = document.createElementNS(this.svgNs, 'stop');
+    const stop2 = document.createElementNS(this.svgNs, 'stop');
+
+    linearGradientTag.setAttribute('id', 'paint1');
+    linearGradientTag.setAttribute('gradientTransform', 'rotate(90)');
+
+    stop1.setAttribute('stop-color', '#FA00FF');
+
+    stop2.setAttribute('offset', '1');
+    stop2.setAttribute('stop-color', '#0085FF');
+
+    linearGradientTag.appendChild(stop1);
+    linearGradientTag.appendChild(stop2);
+
+    defsTagOfColor.appendChild(linearGradientTag);
+
+    this.chart.appendChild(defsTagOfColor);
+
     // make g container
-    let gTagOfPolyLine = document.createElementNS(this.svgNs, 'g');
+    const gTagOfPolyLine = document.createElementNS(this.svgNs, 'g');
     gTagOfPolyLine.classList.add('datas');
 
     for (let i = 0; i < this.datas.length; i++) {
       let points = this.datas[i].data
         .map((value, j) => {
-          let x = ((j + 1) / this.xAxisCount) * (this.width - this.padding.y);
-          let y = (this.hegiht - this.padding.x) * (value / this.maxData);
+          let x =
+            (j / (this.xAxisCount - 1)) * (this.width - this.padding.y) +
+            this.padding.y;
+          let y =
+            this.hegiht -
+            this.padding.x -
+            (this.hegiht - this.padding.x) * (value / this.maxData);
 
           return `${x},${y}`;
         })
@@ -142,9 +172,15 @@ class Chart {
       // draw polylines
       const polyLine = document.createElementNS(this.svgNs, 'polyline');
       polyLine.setAttribute('points', points);
-      polyLine.setAttribute('stroke', this.datas[i].color);
+      if (i === 0) {
+        polyLine.setAttribute('stroke', "url('#paint1')");
+      } else {
+        polyLine.setAttribute('stroke', this.datas[i].color);
+      }
       polyLine.setAttribute('fill', 'none');
       polyLine.setAttribute('stroke-width', this.datas[i].weight + '');
+      polyLine.setAttribute('stroke-linecap', 'round');
+      polyLine.setAttribute('stroke-linejoin', 'round');
 
       gTagOfPolyLine.appendChild(polyLine);
     }
@@ -152,7 +188,79 @@ class Chart {
     this.chart.appendChild(gTagOfPolyLine);
   };
 
-  private setLabel = () => {};
+  private setLabel = () => {
+    const gTagOfText = document.createElementNS(this.svgNs, 'g');
+    const gTagOfXLabel = document.createElementNS(this.svgNs, 'g');
+    const gTagOfYLabel = document.createElementNS(this.svgNs, 'g');
+
+    gTagOfText.setAttribute('fill', '#fff');
+    gTagOfText.setAttribute('font-size', this.fontSize + 'px');
+    gTagOfText.classList.add('labels');
+
+    gTagOfXLabel.setAttribute('text-anchor', 'end');
+
+    gTagOfYLabel.setAttribute('text-anchor', 'end');
+
+    // xLabel
+    this.labels.map((label, i) => {
+      let x =
+        (i / (this.xAxisCount - 1)) * (this.width - this.padding.y) +
+        this.padding.y;
+      let y = this.hegiht - this.padding.x + this.fontSize * 2;
+
+      const text = document.createElementNS(this.svgNs, 'text');
+      text.setAttribute('x', x + '');
+      text.setAttribute('y', y + '');
+      text.append(label);
+
+      gTagOfXLabel.appendChild(text);
+    });
+    gTagOfText.appendChild(gTagOfXLabel);
+
+    // yLabel
+    // 1. 10개의 y lable 데이터 생성
+    // 2. x, y좌표 생성
+    for (let i = 0; i <= this.yAxisCount; i++) {
+      let x =
+        this.padding.x - Math.ceil(Math.log(this.maxData + 1) / Math.LN10);
+      let y = (this.hegiht - this.padding.x) * (i / this.yAxisCount);
+      let label =
+        ((this.yAxisCount - i) / this.yAxisCount) *
+          (this.maxData - this.minData) +
+        this.minData;
+
+      const text = document.createElementNS(this.svgNs, 'text');
+      text.setAttribute('x', x + '');
+      text.setAttribute('y', y + '');
+      text.append(label + '');
+
+      gTagOfYLabel.appendChild(text);
+    }
+    gTagOfText.appendChild(gTagOfYLabel);
+
+    this.chart.appendChild(gTagOfText);
+  };
+
+  private setGuideLine = () => {
+    const gTagOfLine = document.createElementNS(this.svgNs, 'g');
+    gTagOfLine.setAttribute('stroke', '#fff');
+    gTagOfLine.setAttribute('stroke-weight', '1');
+    for (let i = 0; i <= this.yAxisCount; i++) {
+      const x1 = this.padding.y;
+      const x2 = this.width;
+      const y = (this.hegiht - this.padding.x) * (i / this.yAxisCount);
+
+      const line = document.createElementNS(this.svgNs, 'line');
+      line.setAttribute('x1', x1 + '');
+      line.setAttribute('x2', x2 + '');
+      line.setAttribute('y1', y + '');
+      line.setAttribute('y2', y + '');
+
+      gTagOfLine.appendChild(line);
+    }
+
+    this.chart.appendChild(gTagOfLine);
+  };
 
   // rendering for chart
   public render = () => {
@@ -163,6 +271,15 @@ class Chart {
     this.setPoints();
 
     // 데이터 라벨링
+    this.setLabel();
+
+    // 가이드 라인
+    this.setGuideLine();
+
+    // 라벨 박스
+
+    // last point
+
     document.getElementById(this.targetId)?.appendChild(this.chart);
   };
 }
